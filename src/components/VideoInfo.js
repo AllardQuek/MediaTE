998f70569a6c72ade6d53fc4eb5d6e461bb02d7f
Allard <AllardQuek@users.noreply.github.com>
2021-07-23 20:02:33 +0800

    added new components

---
+++ src/components/VideoInfo.js
@@ -0,0 +1,13 @@
+const VideoInfo = () => {
+  return (
+    <div>
+      <p>
+        Max Video Size: <strong>10MB</strong>
+      </p>
+      <p>Video must have a face in every frame!</p>
+      <p>We recommend a short video, less than 10s will do! :)</p>
+    </div>
+  );
+};
+
+export default VideoInfo;
